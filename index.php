bec7ba0a7d099b26a5ff42d9451b790bd4cf9e98
Briyith <portillobriyith2@gmail.com>
2023-01-14 18:46:00 -0400

    styles-added

--- index.php
+++ index.php
@@ -12,26 +12,59 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles/index.css">
+ <link rel="stylesheet" href="style.css">
+
   <title>Document</title>
 </head>
 <body>
   <div class="container">
-    <form action="insert.php" method="POST" class="form">
-      <div class="title">
-        <h1>Create Users</h1>
-      </div>
-      <div class="input">
-        <input type="text" name="name" placeholder="Name">
-        <input type="text" name="lastname" placeholder="Lastname">
-        <input type="text" name="username" placeholder="Username">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-      </div>
-      <div class="button">
-        <button type="submit">Create</button>
-    </form>
-    <button><a href="registered.php">show</a></button>
+    <div class="son">
+      <form action="insert.php" method="POST" class="form">
+          <h1>Create Users</h1>
+        <div class="input">
+          <div class="row">
+            <div class="col">
+              <input 
+              type="text"
+              name="name" 
+              autocomplete="off"
+              placeholder="Name"
+              >
+            </div>
+            <div class="col">
+              <input 
+              type="text" 
+              name="lastname" 
+              autocomplete="off"
+              placeholder="Lastname"
+              >
+            </div>
+          </div>
+
+          <input 
+          type="text" 
+          name="username"
+          autocomplete="off"
+          placeholder="Username"
+              >
+          </div>
+          <input 
+          type="email" 
+          name="email" 
+          autocomplete="off"
+          placeholder="Email"
+          >
+          <input 
+          type="password" 
+          name="password" 
+          autocomplete="off"
+          placeholder="Password"
+          >
+        </div>
+          <button type="submit" class="btn"><a href="index.php">Create</a></button>
+        </form>
+        <button type="submit" class="btn"><a href="registered.php">show</a></button>
+
+    </div>
   </div>
-</body>
 </html>
