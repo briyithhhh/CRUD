33b74d39c8bd58ae0f934c3e510e2abc5e4066cf
Briyith <portillobriyith2@gmail.com>
2023-01-25 14:00:06 -0400

    crud-finished

--- update.php
+++ update.php
@@ -20,7 +20,7 @@ if (isset($id)) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="edit.css">
+  <link rel="stylesheet" href="styles/edit.css">
   <title>Document</title>
 </head>
 <body>
